3d9e5b89c004ef59f8b0cc1a6a37df9b1b4578b7
sansui <1822493066@qq.com>
2024-10-02 16:42:53 +0800

    xiaoyu

---
+++ src/main/java/com/qst/medical/MedicalApplication.java
@@ -0,0 +1,12 @@
+package com.qst.medical;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value = "com.qst.medical.mapper")
+public class MedicalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalApplication.class, args);
+    }
+}
